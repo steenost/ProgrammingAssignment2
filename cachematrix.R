d6ee2e00bd227246980b8df01e890e174f905da1
steenost <stos@privat.dk>
2014-10-25 14:16:23 +0200

    bettter  documentation

--- cachematrix.R
+++ cachematrix.R
@@ -5,18 +5,18 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## set the value of the matrix
-  i<- NULL
+  ## set the value of the matrix m
+  m <- NULL
   set <- function(y) {
     x <<- y
-    i <<- NULL
+    m <<- NULL
   }
   ## get the value of the matrix
   get <- function() x
   ## set the inverse of the matrix
-  setinverse <- function(solve) i <<- solve
-  ## get the inverse of the matrix
-  getinverse <- function() i
+  setinverse <- function(solve) m <<- solve
+  ## get the inverse of the matrix m
+  getinverse <- function() m
  
   list(set = set, get = get,
        setinverse = setinverse,
@@ -29,16 +29,16 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   ## get the inverse of the matrix
-  i <- x$getinverse()
-  ## check if there is the matrix
-  if(!is.null(i)) {
+  m <- x$getinverse()
+  ## control if there is the matrix
+  if(!is.null(m)) {
     message("getting cached data")
-    return(i)
+    return(m)
   }
-  ## if not: get the inverse of the matrix
+  ## no matrix get the inverse of the matrix
   data <- x$get()
-  i <- solve(data, ...)
+  m <- solve(data, ...)
   ## set the inverse of the matrix
-  x$setinverse(i)
-  i
+  x$setinverse(m)
+  m
 }
